f882898a399f024630e96c632aedb151fd5ed67d
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-02-28 16:34:18 +0100

    Add index.js file

---
+++ index.js
@@ -0,0 +1,22 @@
+const toggleButton = document.querySelector('.toggle-button');
+const menuButton = document.querySelector('.x-button');
+const navbarLinks = document.querySelector('.nav-ul');
+// const bodyElement = document.querySelector('body');
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('display-none-sm');
+
+  toggleButton.classList.add('display-none-sm');
+  menuButton.classList.toggle('display-none-sm');
+});
+
+menuButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('display-none-sm');
+
+  toggleButton.classList.toggle('display-none-sm');
+  menuButton.classList.toggle('display-none-sm');
+});
+
+navbarLinks.addEventListener('click', () => {
+  navbarLinks.classList.toggle('display-none-sm');
+});
