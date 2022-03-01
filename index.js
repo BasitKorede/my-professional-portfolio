c90f6ede286d969d666b7e620faaecce3c656b5f
yasin <yasin.elsharawi23@gmail.com>
2022-03-01 13:41:37 +0600

    Add button responsiveness for mobile

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const toggleButton = document.querySelector('.toggle-button');
 const menuButton = document.querySelector('.x-button');
 const navbarLinks = document.querySelector('.nav-ul');
-// const bodyElement = document.querySelector('body');
+const bodyElement = document.querySelector('body');
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('display-none-sm');
-
+  bodyElement.style.overflow = 'hidden';
   toggleButton.classList.add('display-none-sm');
   menuButton.classList.toggle('display-none-sm');
 });
 
 menuButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('display-none-sm');
-
+  bodyElement.style.overflow = 'auto';
   toggleButton.classList.toggle('display-none-sm');
   menuButton.classList.toggle('display-none-sm');
 });
 
 navbarLinks.addEventListener('click', () => {
   navbarLinks.classList.toggle('display-none-sm');
+  toggleButton.classList.toggle('display-none-sm');
+  menuButton.classList.toggle('display-none-sm');
 });
